09e99d291d451984e927ff86ed42b6472b992aa5
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-01 12:44:51 +0530

    Creating : Creating the master branch

---
+++ src/LineComparisonProblem.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LineComparisonProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Comparison Computation Program");
+    }
+}
+
+
+
+
+
